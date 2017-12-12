62c1585c11f783895ceead3d7119b3339b03eb9a
Taysha <taariyadavis@gmail.com>
2017-12-11 22:42:14 -0600

    Finished! Possible additions: sound effects, restart without closing/reopening

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from time import sleep
 from random import randint;
 
@@ -11,6 +12,7 @@ sequence = []
 user_moves = []
 user_turn = False
 game_started = False
+user_wins = False
 #variables for display text
 mode_label_text = "Strict OFF"
 counter_label_text = "--"
@@ -24,7 +26,6 @@ active_colors = ["#e47267", "#7de886", "#7267e4", "#fff280"]
 #blinkon returns it to a normal state
 #flash blinks on, then blinks off after short delay
 #//TODO: add sound effects?
-
 def getTarget(id):
     target = None
     if (id == 0):
@@ -36,13 +37,16 @@ def getTarget(id):
     else:
         target = yellow
     return target
+
 def blinkOn(id):
     target = getTarget(id)
     canvas.itemconfig(target, fill=active_colors[id])
+
 def blinkOff(id):
     target = getTarget(id)
     canvas.itemconfig(target, fill=colors[id])
-def flash (id, delay=3000):
+
+def flash (id, delay=1000):
     blinkOn(id)
     root.update()
     root.after(delay, blinkOff(id))
@@ -75,12 +79,53 @@ def update_counter(operation):
 def display_seq():
     global sequence, counter_label_text, game_started, user_turn
     if game_started:
-        current_count = int(counter_label_text);
+        current_count = int(counter_label_text)
         for x in range(current_count+1):
             flash(sequence[x])
         user_turn = True;
 
+#function to reset the game
+def reset():
+    global user_moves, user_turn, sequence, game_started
+    user_moves = []
+    user_turn = False
+    sequence = []
+    game_started = False
+    update_counter(0)
 
+
+#function to process user input. main game logic
+def user_move(id):
+    global user_turn, user_moves, user_wins, sequence, max_seq_length, counter_label_text, strict_mode, game_started
+    current_count = int(counter_label_text)
+    if (user_turn and len(user_moves) <= max_seq_length):
+        user_moves.append(id)
+        if (id == sequence[len(user_moves) - 1]):
+            #did the player win?
+            #TODO: display winning message
+            if (len(user_moves) == 20):
+                tk.messagebox.showinfo("Congratulations!", "You won.")
+                reset()
+                generate_seq()
+            elif(len(user_moves) == current_count + 1):
+                update_counter(1)
+                user_moves = []
+                user_turn = False
+                display_seq()
+        else:
+            if strict_mode:
+                tk.messagebox.showerror("Oops!", "That was the wrong button...let's begin again.")
+                user_moves = []
+                user_turn = False
+                update_counter(0)
+                sequence = []
+                generate_seq()
+            else:
+                tk.messagebox.showerror("Whoops!", "That was the wrong button. Try it again.")
+                user_moves = []
+                user_turn = False
+                display_seq()
+#function to start the game
 def start_game():
     global game_started
     if (not game_started):
@@ -88,15 +133,21 @@ def start_game():
         update_counter(0)
         generate_seq()
         for x in range(4):
-            flash(x, 2000)
-def game_on():
-    pass
+            flash(x, 1000)
+        display_seq()
+
+
+def green_move(event):
+    user_move(1)
+def red_move(event):
+    user_move(0)
+def blue_move(event):
+    user_move(2)
+def yellow_move(event):
+    user_move(3)
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=800, height=600)
-canvas.pack()
-
-
 
 #-----------------------------these are the buttons-------------------------------------
 green = canvas.create_rectangle(2, 2, 400, 300, fill=colors[1], activefill=active_colors[1], width=2)
@@ -117,9 +168,13 @@ counter_label = canvas.create_text(300, 325, font=("Impact", 20), text="SCORE:",
 count = canvas.create_text(375, 325, font=("Impact", 20), text=counter_label_text, fill="red")
 mode_label = canvas.create_text(475, 325, font=("Impact", 20), text = mode_label_text, fill="white")
 
-canvas.tag_bind(mode_label, '<ButtonPress-1>', toggle_strict)
 
+canvas.tag_bind(mode_label, '<ButtonPress-1>', toggle_strict)
+canvas.tag_bind(green, '<ButtonPress-1>', green_move);
+canvas.tag_bind(yellow, '<ButtonPress-1>', yellow_move);
+canvas.tag_bind(red, '<ButtonPress-1>', red_move);
+canvas.tag_bind(blue, '<ButtonPress-1>', blue_move);
+canvas.pack()
 """issue! even when using tag binds or explicit delays, the program always starts the game immediately upon execution. I've just given up on having any built-in delay for now, but optimal thing would be to get that working"""
 start_game()
-root.after(3000, display_seq)
 root.mainloop()
